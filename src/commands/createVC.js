142237fdd07d1160ed62a8b018f6fa1d81d1de0d
Nico Jensen <mail@njensen.org>
2022-01-31 18:57:07 +0100

    Add Sting option to create channel with own name

--- src/commands/createVC.js
+++ src/commands/createVC.js
@@ -3,18 +3,30 @@ const {MessageEmbed, Guild} = require('discord.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('vc')
-    .setDescription('create new voice channel.'),
+        .setName("vc")
+        .setDescription("create new voice channel.")
+        .addStringOption(option => 
+            option.setName("vcname")
+            .setDescription("Enter your channel Name.")
+            .setRequired(false)),
     async execute(interaction) {
-        const newChannelName = `${interaction.member.displayName}'s Channel`
+        var selectedVCName = interaction.options.getString("vcname")
+        var newChannelName = `${interaction.member.displayName}'s Channel`
+                
+        if (selectedVCName) {
+            newChannelName = selectedVCName
+        }
+
         interaction.guild.channels.create(`${newChannelName}`, {
             type: 'GUILD_VOICE',
-            parent: '589107550520082591',
+            parent: process.env.PARENT_CHANNEL_ID,
             permissionOverwrites: [{
                 id: interaction.member.user,
                 allow: ['MANAGE_CHANNELS']
             }]
         })
-        interaction.reply("Your channel is created.\nYou have all the rights to modify the channel.\nThe channel is automatically deleted after the last user has left it.")
+
+        var replyMsg = "Your channel is created.\nYou have all the rights to modify the channel.\nThe channel is automatically deleted after the last user has left it.";
+        await interaction.reply({ content: replyMsg, ephemeral: true });
     }
 }
